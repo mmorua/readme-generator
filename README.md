3a8f0b9543a431d6abfe72917821443b5ae3256e
Marcus Morua <91860506+mmorua@users.noreply.github.com>
2022-03-01 18:58:13 -0600

    feat(dist): README to generate after running node index.js

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # readme-generator
+    
+## Description 
+An application that will generate a readme
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Clone the repository from GitHub and run npm install from your terminal.
+
+## Usage 
+To use the application after you answer the question prompts a readme will be generated for you under the dist folder.
+
+## License 
+MIT
+
+## Contributing 
+Contributors should read the installation section. 
+
+## Tests
+To test the application just run node index in your terminal. This will start the application!
+
+## Questions
+If you have any questions about this project, please contact me directly at marcus.morua@gmail.com You can view more of my projects at https://github.com/mmorua
--- index.js
+++ index.js
@@ -11,28 +11,28 @@ inquirer.prompt(
         },
         {
             name: 'projectDescription',
-            type: 'confirm',
-            message: 'Do you want to add a description to your project?'
-        }, // When I answer yes or no for this confirm prompt I want to be able to add a description if I choose yes, not return true or false.
+            type: 'input',
+            message: 'What is the description of your project?'
+        }, 
         {
             name: 'projectInstallation',
-            type: 'confirm',
-            message: 'Do you want to add installation instructions to your project?',
+            type: 'input',
+            message: 'What are the installation instructions of your project?'
         },
         {
             name: 'projectUsage',
-            type: 'confirm',
-            message: 'Do you want to add usage instructions to your project?'
+            type: 'input',
+            message: 'What are the usage instructions of your project?'
         },
         {
             name: 'projectLicense',
-            type: 'confirm',
-            message: 'Do you want to add a license to your project?'
+            type: 'input',
+            message: 'What is the license of your project?'
         },
         {
             name: 'projectTests',
-            type: 'confirm',
-            message: 'Would you like to add test instructions to your project?'
+            type: 'input',
+            message: 'What are the test instructions of your project?'
         },
         {
             name: 'projectEmail',
